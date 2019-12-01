14f51a8c2a3575d859985b9e3eb817fd8d9de8a0
Vadim Fedorenko <meiblorn@gmail.com>
2019-12-02 02:41:08 +0400

    Docker. Sub dag execution

--- dags/jobs_dag.py
+++ dags/jobs_dag.py
@@ -3,6 +3,7 @@ import random
 from datetime import datetime
 
 from airflow import DAG
+from airflow.models import TaskInstance
 from airflow.operators.bash_operator import BashOperator
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python_operator import PythonOperator, BranchPythonOperator
@@ -12,30 +13,30 @@ default_args = {
 }
 
 config = {
-    'table_1_extractor': {
+    'database_1_extractor': {
         'schedule_interval': None,
         'start_date': datetime(2019, 11, 30),
-        'table_name': 'table_1'
+        'database': 'database_1'
     },
-    'table_2_processor': {
+    'database_2_processor': {
         'schedule_interval': None,
         'start_date': datetime(2019, 11, 30),
-        'table_name': 'table_2'
+        'database': 'database_2'
     },
-    'table_3_postprocessor': {
+    'database_3_postprocessor': {
         'schedule_interval': None,
         'start_date': datetime(2019, 11, 30),
-        'table_name': 'table_3'
+        'database': 'database_3'
     }
 }
 
 
 def _print_process_start_op() -> PythonOperator:
-    def __callable_fn():
+    def __callable_fn(task_instance: TaskInstance, **context):
         logger = logging.getLogger(__file__)
-        logger.info('{dag_id} start processing tables in database: {database}')
+        logger.info(f'{task_instance.dag_id} started processing tables in database ...')
 
-    return PythonOperator(task_id='print_process_start', python_callable=__callable_fn)
+    return PythonOperator(task_id='print_process_start', python_callable=__callable_fn, provide_context=True)
 
 
 def _get_current_user_op() -> BashOperator:
@@ -69,8 +70,19 @@ def _query_the_table_op(*args, **kwargs) -> DummyOperator:
     return DummyOperator(task_id='query_the_table', *args, **kwargs)
 
 
+def _xcom_push(*args, **kwargs) -> PythonOperator:
+    def __callable_fn(task_instance: TaskInstance, **context):
+        task_instance.xcom_push(key='dag_status', value=f'{context["run_id"]} ended')
+        task_instance.xcom_push(key='execution_date', value=context["execution_date"])
+
+    return PythonOperator(
+        task_id='xcom_push', python_callable=__callable_fn, provide_context=True,
+        *args, **kwargs
+    )
+
+
 for dag_id, dag_config in config.items():
-    with DAG(dag_id, default_args={**default_args, **dag_config}) as dag:
+    with DAG(dag_id, default_args={**default_args, **dag_config}, schedule_interval=None) as dag:
         globals()[dag_id] = dag
 
         # @formatter:off
@@ -78,5 +90,6 @@ for dag_id, dag_config in config.items():
             >> _get_current_user_op() \
             >> _check_table_exists_branch_op() >> [_create_table_op(), _skip_table_creation_op()] \
             >> _insert_new_row_op(trigger_rule='none_failed') \
-            >> _query_the_table_op()
+            >> _query_the_table_op() \
+            >> _xcom_push()
         # @formatter:on
---
+++ dags/trigger_dag.py
@@ -0,0 +1,98 @@
+import os
+from datetime import datetime
+
+from airflow import DAG
+from airflow.contrib.sensors.file_sensor import FileSensor
+from airflow.models import TaskInstance, Variable
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.dagrun_operator import TriggerDagRunOperator
+from airflow.operators.python_operator import PythonOperator
+from airflow.operators.sensors import ExternalTaskSensor
+from airflow.operators.subdag_operator import SubDagOperator
+
+_current_dir = os.path.dirname(__file__)
+
+default_args = {
+    'owner': 'Vadim Fedorenko',
+    'schedule_interval': None,
+    'start_date': datetime(2019, 11, 30),
+}
+
+_triggers_dir = os.path.join(_current_dir, 'triggers')
+_trigger_file = os.path.join(_triggers_dir, 'run')
+
+_finished_files_dir = os.path.join(_current_dir, 'finished')
+
+
+def _file_sensor() -> FileSensor:
+    return FileSensor(task_id='wait_trigger_file', filepath=_trigger_file)
+
+
+def _trigger_dag_op(trigger_dag_id_, *args, **kwargs) -> TriggerDagRunOperator:
+    def __dag_run_obj_fn(context, dro):
+        task_instance: TaskInstance = context['ti']
+        task_instance.xcom_push(key='trigger_dag_id', value=trigger_dag_id_)
+        return dro
+
+    return TriggerDagRunOperator(
+        task_id='trigger_dag', trigger_dag_id=trigger_dag_id_, python_callable=__dag_run_obj_fn,
+        *args, **kwargs
+    )
+
+
+def _process_results_sub_dag_op(parent_dag_id_, trigger_dag_id_) -> SubDagOperator:
+    def _triggered_dag_sensor() -> ExternalTaskSensor:
+        # noinspection PyTypeChecker
+        return ExternalTaskSensor(
+            task_id='sensor_triggered_tag',
+            external_dag_id=trigger_dag_id_,
+            external_task_id=None,
+            allowed_states=['success'],
+            check_existence=True
+        )
+
+    def _print_result_op(*args, **kwargs) -> PythonOperator:
+        def __callable_op(task_instance: TaskInstance, **context):
+            import logging
+            target_dag_status = task_instance.xcom_pull(dag_id=trigger_dag_id_, key='dag_status')
+            logging.getLogger(__file__).info(f'Target DAG status: {target_dag_status}')
+
+        return PythonOperator(
+            task_id='print_result', python_callable=__callable_op, provide_context=True,
+            *args, **kwargs
+        )
+
+    def _remove_trigger_file_op(*args, **kwargs) -> BashOperator:
+        return BashOperator(task_id='remove_trigger_file', bash_command=f'rm {_trigger_file}', *args, **kwargs)
+
+    def _create_finished_ts(*args, **kwargs) -> BashOperator:
+        return BashOperator(
+            task_id='create_finished_timestamp',
+            bash_command='touch %s/finished_{{ ds_nodash }}' % _finished_files_dir,
+            *args, **kwargs
+        )
+
+    sub_dag = DAG(f'{parent_dag_id_}.process_results', default_args=default_args, schedule_interval=None)
+
+    with sub_dag:
+        # @formatter:off
+        _triggered_dag_sensor() \
+            >> _print_result_op() \
+            >> _remove_trigger_file_op() \
+            >> _create_finished_ts()
+        # @formatter:on
+
+    return SubDagOperator(task_id='process_results', subdag=sub_dag)
+
+
+dag_id = 'trigger_dag'
+with DAG(dag_id, default_args=default_args, schedule_interval=None) as dag:
+    trigger_dag_id = Variable.get('dag_id', 'database_1_extractor')
+
+    # @formatter:off
+    _file_sensor() \
+        >> _trigger_dag_op(trigger_dag_id_=trigger_dag_id, execution_date='{{execution_date}}') \
+        >> _process_results_sub_dag_op(parent_dag_id_=dag_id, trigger_dag_id_=trigger_dag_id)
+    # @formatter:on
+
+    globals()[dag_id] = dag
